e7768584cd1ee4a87db51a6ccfa763eafefc28e8
Tarun Narahari <tarun.narahari@gmail.com>
2022-11-26 15:17:58 -0800

    express

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+const express = require("express");
+// Initialize express
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// parse JSON
+app.use(express.json());
+// parse URL encoded data
+app.use(express.urlencoded({ extended: true }));
+//This method returns all  the users in database
+app.get("/", (req,res) => {
+  res.json({ message: "Hello WorlD." });
+});
+
+
+
